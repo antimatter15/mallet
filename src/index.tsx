9ba84e2ed24fc9e0ae793af79a4e25e0d247a849
Kevin Kwok <antimatter15@gmail.com>
2020-01-02 15:59:07 -0500

    switching to parcel

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.less'
+
+function App() {
+    return <div>hello</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
